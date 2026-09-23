07d5c41f5407f57d96818d71c2035e9906cf3f95
Sheyda Kate <sheydakate@gmail.com>
2026-09-23 11:26:33 -0400

    Fix page refresh on GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react' //React development tool to detect problems
 import { createRoot } from 'react-dom/client' //R DOM function - creates a root for rendering components in browser
 import './index.css' //Loads global CSS styles
 import App from './App.jsx' //my main Rcomponent, defined in App.jsx
-import { BrowserRouter } from 'react-router-dom'
-
+import { HashRouter } from 'react-router-dom'
 /*
   document -> webpage loaded in browser
   getElementById('root') -> searches the HTML doc for an element whose id is root
@@ -11,8 +10,8 @@ import { BrowserRouter } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
  //tells react what to display
   <StrictMode>
-    <BrowserRouter  basename="/portfolio">
-        <App />
-    </BrowserRouter>
+    <HashRouter>        
+      <App />
+    </HashRouter>
   </StrictMode>,
 )
